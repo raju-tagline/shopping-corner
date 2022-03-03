1488f39daa17fddfb67df22189620653d59c7a40
Raju Tagline <raju.tagline@gmail.com>
2022-03-03 12:14:17 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shopping-corner-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'shopping-corner';
+}
