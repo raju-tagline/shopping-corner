10080318e16ee6f2dc2171f23c3c3902c7aa879e
Raju Tagline <raju.tagline@gmail.com>
2022-03-03 14:39:45 +0530

    new footer component created into core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BackButtonComponent } from 'src/app/core/back-button/back-button.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [BackButtonComponent, HeaderComponent],
+  declarations: [BackButtonComponent, HeaderComponent, FooterComponent],
   imports: [CommonModule],
-  exports: [BackButtonComponent, HeaderComponent],
+  exports: [BackButtonComponent, HeaderComponent, FooterComponent],
 })
 export class CoreModule {}
