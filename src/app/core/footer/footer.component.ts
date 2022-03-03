e0aaee49066c9dc824279e5f4aa902ea399aa43c
Raju Tagline <raju.tagline@gmail.com>
2022-03-03 15:41:52 +0530

    added css into footer component of coere module

--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'shopping-corner-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+  public year: Date = new Date();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
