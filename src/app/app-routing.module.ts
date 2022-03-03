9f91d8c9619479457bd0885615714cf7039cad96
Raju Tagline <raju.tagline@gmail.com>
2022-03-03 17:03:36 +0530

    new module user created with routing into feature folder and routing of user module is given into app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'users',
+    loadChildren: () =>
+      import('./feature/user/user.module').then((m) => m.UserModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
