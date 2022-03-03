0c58cf412cc2b2a94c38d5679a39ecadcf428fed
Raju Tagline <raju.tagline@gmail.com>
2022-03-03 12:44:17 +0530

    change path style into angular json's architect

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BackButtonComponent } from './back-button/back-button.component';
+import { BackButtonComponent } from 'src/app/core/back-button/back-button.component';
 
 
 @NgModule({
