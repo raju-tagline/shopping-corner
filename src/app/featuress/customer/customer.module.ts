c79fd1b3767bd989317e835e34e241bc11681dc7
Raju Tagline <raju.tagline@gmail.com>
2022-03-03 17:32:27 +0530

    src path added into customer module

---
+++ src/app/featuress/customer/customer.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CustomerRoutingModule } from 'src/app/features/customer/customer-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CustomerRoutingModule
+  ]
+})
+export class CustomerModule { }
