ad9ac5c5c5170b4ef5c146a871b7969970325273
Raju Tagline <raju.tagline@gmail.com>
2022-03-07 14:24:07 +0530

    customer service feedback form completed

--- src/app/features/customer/customer-service/customer-service.component.ts
+++ src/app/features/customer/customer-service/customer-service.component.ts
@@ -1,15 +1,28 @@
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'shopping-corner-customer-service',
   templateUrl: './customer-service.component.html',
-  styleUrls: ['./customer-service.component.scss']
+  styleUrls: ['./customer-service.component.scss'],
 })
 export class CustomerServiceComponent implements OnInit {
+  public feedBackForm!: FormGroup;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.feedBackForm = new FormGroup({
+      name: new FormControl(null, Validators.required),
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$'),
+      ]),
+      feedBack: new FormControl(null, Validators.required),
+    });
   }
-
+  public onSumbit(): void {
+    console.log('this.feedBackForm.value :>> ', this.feedBackForm.value);
+    this.feedBackForm.reset();
+  }
 }
--- src/app/features/customer/customer.module.ts
+++ src/app/features/customer/customer.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CustomerRoutingModule } from 'src/app/features/customer/customer-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { DealsComponent } from './deals/deals.component';
@@ -9,7 +9,18 @@ import { MobilesComponent } from './mobiles/mobiles.component';
 import { CustomerServiceComponent } from './customer-service/customer-service.component';
 
 @NgModule({
-  declarations: [DashboardComponent, DealsComponent, BooksComponent, MobilesComponent, CustomerServiceComponent],
-  imports: [CommonModule, CustomerRoutingModule, FormsModule],
+  declarations: [
+    DashboardComponent,
+    DealsComponent,
+    BooksComponent,
+    MobilesComponent,
+    CustomerServiceComponent,
+  ],
+  imports: [
+    CommonModule,
+    CustomerRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
 export class CustomerModule {}
