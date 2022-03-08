171a8783b77d7a9534faa31126e53dadbdab9c65
Raju Tagline <raju.tagline@gmail.com>
2022-03-08 11:26:41 +0530

    for api call first from firebase done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
+import { DatabaseService } from 'src/app/services/database.service';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'shopping-corner-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'shopping-corner';
+  constructor(private databaseService: DatabaseService) {
+
+    //admin
+    this.databaseService.getUserLogin().subscribe((res) => {
+      console.log('res :>> ', res);
+    })
+
+    //user
+    this.databaseService.get().subscribe((res) => {
+      console.log('res :>> ', res);
+    })
+  }
 }
--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { IUserSignUp } from 'src/app/core/interface/user.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DatabaseService {
+  public url: string = environment.firebaseConfig.databaseUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  public getUserLogin(): Observable<any> {
+    return this.http.get(`${this.url}/admin.json`);
+  }
+
+  public getUserSignUp(userData: IUserSignUp): Observable<IUserSignUp> {
+    return this.http.post<IUserSignUp>(`${this.url}/user.json`, userData);
+  }
+
+  public get(): Observable<any> {
+    return this.http.get(`${this.url}/user.json`);
+  }
 }
