74ee3939ffc10594f0253ddd4e6531ba0c9fd626
Raju Tagline <raju.tagline@gmail.com>
2022-03-08 15:17:18 +0530

    new auth service for user login with firebase authentication

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private angularFireAuth: AngularFireAuth) {}
+
+  public login(email: string, password: string) {
+    console.log('password :>> ', password);
+    console.log('email :>> ', email);
+
+    this.angularFireAuth
+      .signInWithEmailAndPassword(email, password)
+      .then((value) => {
+        console.log('value :>> ', value);
+      })
+      .catch((err) => {
+        console.log('Something went wrong:', err.message);
+      });
+  }
+
+  public logout() {
+    this.angularFireAuth.signOut();
+  }
+}
