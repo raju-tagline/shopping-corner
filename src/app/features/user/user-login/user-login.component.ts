75911d2c154e722272dae618ac2dae317763a22e
Raju Tagline <raju.tagline@gmail.com>
2022-03-08 15:17:52 +0530

    auth service implemented into user login

--- src/app/features/user/user-login/user-login.component.ts
+++ src/app/features/user/user-login/user-login.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'shopping-corner-user-login',
@@ -10,23 +11,26 @@ export class UserLoginComponent implements OnInit {
   @Input() public websiteName?: string = 'Shopping Corner';
   public myLoginForm!: FormGroup;
 
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
     this.myLoginForm = new FormGroup({
       email: new FormControl(null, [
         Validators.required,
         Validators.email,
-        Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$'),
+        // Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$'),
       ]),
       password: new FormControl(null, [
         Validators.required,
-        Validators.pattern('^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{6,32}$'),
+        // Validators.pattern('^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{6,32}$'),
       ]),
     });
   }
 
   public onSubmit() {
     console.log('this.myLoginForm.value :>> ', this.myLoginForm.value);
+    const email = this.myLoginForm.value.email;
+    const password = this.myLoginForm.value.password;
+    this.authService.login(email, password);
   }
 }
