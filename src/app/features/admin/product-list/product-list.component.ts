6cd1fe9cf289c9388dc59a1f67e5805bb89b746c
Raju Tagline <raju.tagline@gmail.com>
2022-03-09 09:51:22 +0530

    in product list component create design for component and add some static data

--- src/app/features/admin/product-list/product-list.component.ts
+++ src/app/features/admin/product-list/product-list.component.ts
@@ -3,13 +3,57 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'shopping-corner-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.scss']
+  styleUrls: ['./product-list.component.scss'],
 })
 export class ProductListComponent implements OnInit {
+  public productList = [
+    {
+      key: 'auther1product1',
+      name: 'product-1',
+      price: 100,
+      auther: 'tester',
+      Description: 'there is the first every product',
+    },
+    {
+      key: 'auther1product2',
+      name: 'product-2',
+      price: 200,
+      auther: 'tester',
+      Description: 'there is the first every product',
+    },
+    {
+      key: 'auther1product3',
+      name: 'product-3',
+      price: 300,
+      auther: 'tester',
+      Description: 'there is the first every product',
+    },
+    {
+      key: 'auther1product4',
+      name: 'product-4',
+      price: 400,
+      auther: 'tester',
+      Description: 'there is the first every product',
+    },
+    {
+      key: 'auther1product5',
+      name: 'product-5',
+      price: 500,
+      auther: 'tester',
+      Description: 'there is the first every product',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public editProduct(key: string): void {
+    console.log('EDIT :>> ', key);
   }
 
+  public removeProduct(key: string): void {
+    window.alert('Are you sure you want delete');
+    console.log('REMOVE :>> ', key);
+  }
 }
