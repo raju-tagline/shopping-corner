6929b407941c8a191be0232bcfd2a4ee420305c2
Raju Tagline <raju.tagline@gmail.com>
2022-03-10 10:26:37 +0530

    customer logout implemented into customer module

--- src/app/features/customer/customer-navbar/customer-navbar.component.ts
+++ src/app/features/customer/customer-navbar/customer-navbar.component.ts
@@ -1,15 +1,23 @@
+import { AuthService } from 'src/app/services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'shopping-corner-customer-navbar',
   templateUrl: './customer-navbar.component.html',
-  styleUrls: ['./customer-navbar.component.scss']
+  styleUrls: ['./customer-navbar.component.scss'],
 })
 export class CustomerNavbarComponent implements OnInit {
+  public isLogedIn:boolean = false;
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public logOut(): void {
+    this.authService.logout();
+    this.router.navigate(['/user']);
+    localStorage.removeItem('customerId');
+    this.isLogedIn = true;
   }
-
 }
