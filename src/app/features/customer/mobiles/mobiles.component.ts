65b54225663c7296250fdf33cf0a7bd9238e22e0
Raju Tagline <raju.tagline@gmail.com>
2022-03-10 10:27:57 +0530

    mobile product show into website useing fireabase

--- src/app/features/customer/mobiles/mobiles.component.ts
+++ src/app/features/customer/mobiles/mobiles.component.ts
@@ -1,15 +1,27 @@
+import { ProductService } from './../../../services/product.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'shopping-corner-mobiles',
   templateUrl: './mobiles.component.html',
-  styleUrls: ['./mobiles.component.scss']
+  styleUrls: ['./mobiles.component.scss'],
 })
 export class MobilesComponent implements OnInit {
-
-  constructor() { }
+  public mobileProducts:any = []
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
+    this.getProducts();
+   }
+  
+  /**
+   * getProducts
+   */
+  public getProducts() {
+    this.productService.getProduct().then((products:any) => {
+      this.mobileProducts = products?.filter((product:any) => product.category === 'Phones' );
+      console.log('products :>> ', products);
+      console.log('this.mobileProducts :>> ', this.mobileProducts);
+    })
   }
-
 }
