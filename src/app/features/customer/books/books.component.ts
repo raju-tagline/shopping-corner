d58d040147fca149735b11efa395dfbe0ff60792
Raju Tagline <raju.tagline@gmail.com>
2022-03-10 10:24:33 +0530

    all book product are shown form firebase

--- src/app/features/customer/books/books.component.ts
+++ src/app/features/customer/books/books.component.ts
@@ -1,15 +1,30 @@
+import { ProductService } from 'src/app/services/product.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'shopping-corner-books',
   templateUrl: './books.component.html',
-  styleUrls: ['./books.component.scss']
+  styleUrls: ['./books.component.scss'],
 })
 export class BooksComponent implements OnInit {
+  public bookProducts:any=[]
 
-  constructor() { }
+  constructor(private productService:ProductService) {}
 
   ngOnInit(): void {
+    this.getProducts();
   }
 
+  /**
+   * getProducts
+   */
+  public getProducts() {
+    this.productService.getProduct().then((products: any) => {
+      this.bookProducts = products?.filter(
+        (product: any) => product.category === 'Books'
+      );
+      console.log('products :>> ', products);
+      console.log('this.bookProducts :>> ', this.bookProducts);
+    });
+  }
 }
