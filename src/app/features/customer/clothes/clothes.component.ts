7386780f444a1d8453ef3c30a9fd48674871fd7c
Raju Tagline <raju.tagline@gmail.com>
2022-03-10 10:25:02 +0530

    clothe are show from firebasde

--- src/app/features/customer/clothes/clothes.component.ts
+++ src/app/features/customer/clothes/clothes.component.ts
@@ -1,15 +1,27 @@
+import { ProductService } from 'src/app/services/product.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'shopping-corner-clothes',
   templateUrl: './clothes.component.html',
-  styleUrls: ['./clothes.component.scss']
+  styleUrls: ['./clothes.component.scss'],
 })
 export class ClothesComponent implements OnInit {
+  public clotheProduct: any = [];
 
-  constructor() { }
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
+    this.getProducts();
   }
 
+  public getProducts() {
+    this.productService.getProduct().then((products: any) => {
+      this.clotheProduct = products?.filter(
+        (product: any) => product.category === 'Clothe'
+      );
+      console.log('products :>> ', products);
+      console.log('this.clotheProduct :>> ', this.clotheProduct);
+    });
+  }
 }
