0e1dae2594fc0a42b049a6cb59660445e9873e52
raju-tagline <raju.tagline@gmail.com>
2022-03-19 21:06:22 +0530

    show customer profile data into profile component

--- src/app/features/customer/customer-profile/customer-profile.component.ts
+++ src/app/features/customer/customer-profile/customer-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ProfileService } from 'src/app/services/profile.service';
 
 @Component({
   selector: 'shopping-corner-customer-profile',
@@ -6,10 +8,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer-profile.component.scss']
 })
 export class CustomerProfileComponent implements OnInit {
+  public userProfile: any = [];
+  public userProfileForm!: FormGroup;
 
-  constructor() { }
+  constructor(private profileService: ProfileService) {
+  }
 
   ngOnInit(): void {
+    this.userProfileForm = new FormGroup({
+      'name': new FormControl(),
+      'email': new FormControl(),
+      'address': new FormControl(),
+      'gender': new FormControl()
+    });
+    this.userInfo();
+  }
+
+  /**
+   * userInfo
+   */
+  public userInfo() {
+    this.profileService.getUserInfo().then((res: any) => {
+      if (res[1]?.role === "customer") {
+        this.userProfileForm.setValue({
+          name: res[1].name,
+          address: res[1]?.address,
+          email: res[1]?.email,
+          gender: res[1]?.gender,
+        })
+        this.userProfile = res[1]
+      }
+    })
+  }
+
+  /**
+   * onSumbit
+   */
+  public onSumbit() {
+    console.log("This is Profile!!");
   }
 
 }
--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
+  public basePath = this.db.database.ref('/user');
 
-  constructor() { }
+
+  constructor(private db: AngularFireDatabase) { 
+    // this.getUserInfo();
+  }
+
+  public getUserInfo(): any {
+    return new Promise((resolve, reject) => {
+      this.basePath.on('value', (data: any) => {
+        const userProfile = Object.keys(data.val()).map((key) => {
+          return {
+            ...data.val()[key],
+            userId: key,
+          };
+        });
+        resolve(userProfile)
+      });
+    });
+  }
+
 }
