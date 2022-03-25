4bc3e71ce1a19829154c127370159ee8da81acea
raju-tagline <raju.tagline@gmail.com>
2022-03-25 16:53:40 +0530

    Get all the data from firebase and show them into deal component

--- src/app/features/customer/deals/deals.component.ts
+++ src/app/features/customer/deals/deals.component.ts
@@ -1,15 +1,20 @@
+import { ProductService } from './../../../services/product.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'shopping-corner-deals',
   templateUrl: './deals.component.html',
-  styleUrls: ['./deals.component.scss']
+  styleUrls: ['./deals.component.scss'],
 })
 export class DealsComponent implements OnInit {
+  public allProductDetails: any = [];
 
-  constructor() { }
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
+    this.productService.getProduct().then((res) => {
+      this.allProductDetails = res;
+      console.log('TODAY RESPONSE :>> ', res);
+    });
   }
-
 }
