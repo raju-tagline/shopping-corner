fc974b47e602f25631f177be72569fcde8e3ee27
raju-tagline <raju.tagline@gmail.com>
2022-07-25 10:39:26 +0530

    Firebase change for env and firebase json

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyA-Q9-Q8iAy1xvrYFyrbpiGx68aHz1oCZs',
+    authDomain: 'shopping-corner-10966.firebaseapp.com',
+    projectId: 'shopping-corner-10966',
+    storageBucket: 'shopping-corner-10966.appspot.com',
+    messagingSenderId: '669556172046',
+    appId: '1:669556172046:web:55a37bc5537266dbda235f',
+    measurementId: 'G-JXT4N1TPC0',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyA-Q9-Q8iAy1xvrYFyrbpiGx68aHz1oCZs',
+    authDomain: 'shopping-corner-10966.firebaseapp.com',
+    projectId: 'shopping-corner-10966',
+    storageBucket: 'shopping-corner-10966.appspot.com',
+    messagingSenderId: '669556172046',
+    appId: '1:669556172046:web:55a37bc5537266dbda235f',
+    measurementId: 'G-JXT4N1TPC0',
+  },
 };
 
 /*
